f6ee224ced900def912498f403112e50b8525b89
Angie Duncan <60791666+angieduncan@users.noreply.github.com>
2021-08-20 16:01:29 -0500

    Add files via upload

---
+++ Palindromo/Palindromo/Palindromo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    class Palindromo
+    { public string Pharse { get; set };
+        public Boolean EstadoPalindromo { get; set };
+
+        public Palindromo()
+        {
+            Pharse = "";
+            EstadoPalindromo = true;
+        }
+
+       
+        public void ValidarPalindromo()
+        {
+            Array p = Pharse.ToCharArray();
+            for(int i=0; i<p.Length - 1;i++)
+            {
+                if (p[i].Equals( p.Length - 1 - i))
+                {
+                    EstadoPalindromo = true;
+                }
+                else
+                {
+                    EstadoPalindromo = false;
+                    c = c + 1;
+
+                }
+            }
+        }
+
+
+    }
+}
